900374914d36ba72bcc8eabfc434896a25da7082
Andalon <dg4christ08@gmail.com>
2014-03-17 14:59:21 -0400

    Uploading the TeamBots simulator with our project.

---
+++ src/com/robotsagentshumans/assignment/HelloAssignment.java
@@ -0,0 +1,15 @@
+package com.robotsagentshumans.assignment;
+
+import java.io.File;
+
+import TBSim.TBSim;
+
+public class HelloAssignment {
+
+	public static void main(String[] args)
+	{
+		File dscFile = new File("walls.dsc");
+		TBSim simulator = new TBSim();
+		
+	}
+}
