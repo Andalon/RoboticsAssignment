b6a99b287fb35f7dea7dfde8709096dffe25ea4a
DGriffen <david.griffen@knights.ucf.edu>
2014-03-19 00:05:34 -0400

    Added some basic Getter/Setters for the GridWorld

--- src/com/robotsagentshumans/assignment/GridWorld.java
+++ src/com/robotsagentshumans/assignment/GridWorld.java
@@ -1,5 +1,7 @@
 package com.robotsagentshumans.assignment;
 
+import java.awt.Point;
+
 /**
  * GridWorld used for the Robotics Simulation. It is statically designed to be a 10x10 grid, recommended to use a simulation 
  * environment that is an nxn size.
@@ -8,8 +10,49 @@ package com.robotsagentshumans.assignment;
  */
 public class GridWorld {
 
+	private boolean goalStateReached;
+	private int currentState;
+	private int goalState;
+	
 	public GridWorld()
-	{
+	{  
 		
 	}
+	
+	
+	/*
+	 * GETTER/SETTERS for the GridWorld.
+	 */
+	
+	/*
+	 * Returns whether or not the robot has reached the goal state.
+	 */
+	public boolean goalStateReached()
+	{
+		return goalStateReached;
+	}
+	public int getGoalState()
+	{
+		return goalState;
+	}
+	public int getCurrentState()
+	{
+		//return current state.
+		return currentState;
+	}
+	public Point getXYCoordinatesofState(int state)
+	{
+		Point temp = new Point();
+		//temp.X = state.X;
+		//temp.Y = state.Y;
+		return temp; 
+	}
+	public int getHeuristicState(int state, String moveDirection)
+	{
+		int nextState=0;
+		return nextState;
+	}
+	
+	
+	
 }
